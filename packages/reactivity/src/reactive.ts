ea602b6e5b5246d8da50be2ddd7f30e5c6114d1a
tiankai <tiankai@chinatelecom.cn>
2025-10-30 21:05:19 +0800

    feat(reactivity): 实现基础响应式系统(day03) - 添加 reactive 函数创建响应式对象 - 引入 isObject 工具函数进行类型检查 - 实现 Proxy 拦截 get/set 操作 - 添加缓存机制避免重复代理 - 定义 ReactiveFlags 枚举标识响应式状态 - 创建 mutableHandlers 处理器对象

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,3 +1,45 @@
-export function reactive() {
+import {isObject} from "@vue/shared";
+
+export function reactive(target) {
+    return createReactiveObject(target)
+}
+
+const reavtiveMap = new WeakMap()
+enum ReactiveFlags {
+    IS_REACTIVE = '__v_isReactive'
+}
+
+const mutableHandlers:  ProxyHandler<any> = {
+    get(target, key, receiver) {
+        if (key === ReactiveFlags.IS_REACTIVE) {
+            return true
+        }
+    },
+    set(target, key, value, receiver) {
+        return true
+    }
+}
+
+function createReactiveObject(target) {
+    // 统一做判断，响应式对象必须是对象才可以
+    if (!isObject(target)) {
+        return target
+    }
+    // 如果是响应式的，直接返回
+    if (target[ReactiveFlags.IS_REACTIVE]) {
+        return target
+    }
+
+    let proxy = new Proxy(target, mutableHandlers)
+
+    // 取缓存，如果有则直接返回
+    const exitsProxy = reavtiveMap.get(target)
+    if (exitsProxy) {
+        return exitsProxy
+    }
+
+    // 存进缓存
+    reavtiveMap.set(target, proxy)
+    return proxy
 
 }
