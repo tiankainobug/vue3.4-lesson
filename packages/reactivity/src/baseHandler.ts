c4f127030862d424ecd10007b8f939b8ef6df754
tiankai <tiankai@chinatelecom.cn>
2025-11-04 21:52:38 +0800

    refactor(reactivity): 使用 Reflect 优化响应式代理处理器 - 在 get 和 set 操作中引入 Reflect API - 确保代理行为与原始对象操作一致 - 提升代码的可读性和维护性

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -2,13 +2,15 @@ export enum ReactiveFlags {
     IS_REACTIVE = '__v_isReactive'
 }
 
+// proxy 需要搭配 reflect 来使用
 export const mutableHandlers:  ProxyHandler<any> = {
     get(target, key, receiver) {
         if (key === ReactiveFlags.IS_REACTIVE) {
             return true
         }
+        return Reflect.get(target, key, receiver)
     },
     set(target, key, value, receiver) {
-        return true
+        return Reflect.set(target, key, value, receiver)
     }
 }
